8924850594377ed162655c6dda204aa2978e0c4b
Kevin-Leguiza-Gaggero <nicolaskevinleguiza@gmail.com>
2022-05-16 16:59:55 +0000

    update: modify config files

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,16 +1,17 @@
-const esbuild = require("esbuild");
+const esbuild = require('esbuild');
 
-const { nodeExternalsPlugin } = require("esbuild-node-externals");
+const { nodeExternalsPlugin } = require('esbuild-node-externals');
 
 esbuild
   .build({
-    entryPoints: ["./src/index.ts"],
-    outfile: "dist/index.js",
+    entryPoints: ['./src/main.ts'],
+    outfile: 'dist/bundle.js',
     bundle: true,
     minify: true,
-    platform: "node",
+    platform: 'node',
     sourcemap: true,
-    target: "node14",
+    target: 'node14',
+    tsconfig: './tsconfig.json',
     plugins: [nodeExternalsPlugin()],
   })
   .catch(() => process.exit(1));
